1754572a8a3aa44bb164393ed6d245f590dd190e
nathaneleven <nathan.martins@elevenrush.com>
2021-11-08 16:24:06 -0300

    Alterações LP Verizon

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,22 @@
 import Header from '../src/components/molecules/Header'
 import Footer from '../src/components/molecules/Footer'
 import Hero from '../src/components/organisms/Hero'
-import BodyContent from '../src/components/molecules/BodyContent'
+import Cta from '../src/components/organisms/Cta'
+import Plans from '../src/components/organisms/Plans'
+import About from '../src/components/organisms/About'
+import Services from '../src/components/organisms/Services'
+import CtaFooter from '../src/components/organisms/CtaFooter'
 
 export default function Home() {
   return (
     <div>
       <Header />
       <Hero />
-      <BodyContent/>
+      <Cta />
+      <Plans/>
+      <About/>
+      <Services/>
+      <CtaFooter/>
       <Footer/>
     </div>
   )
--- src/components/organisms/About.jsx
+++ src/components/organisms/About.jsx
@@ -11,17 +11,17 @@ const About = () => {
                 <Image borderRadius={{md: "0 0 70px 0"}} src="/about-verizon-image.jpg" alt="Hero Image Verizon" />
             </AspectRatio>
             
-            <Box ml="25px">
-        <Heading mb="24px" >
-            About Verizon 
-        </Heading>
-        <Text>
-            Verizon was founded on June 30, 2000, in the United States, through the merger of Bell Atlantic and GTE Corps. Today, the company is one of the world's leading telecommunications companies.<br/><br/>
+            <Box m="25px">
+                <Heading mb="24px" >
+                    About Verizon 
+                </Heading>
+                <Text>
+                    Verizon was founded on June 30, 2000, in the United States, through the merger of Bell Atlantic and GTE Corps. Today, the company is one of the world's leading telecommunications companies.<br/><br/>
 
-            Present in more than 2.700 cities in the United States and abroad, the company provides internet, cable TV, landline and mobile telephony services. In addition, Verizon has been investing its efforts in interactive entertainment, digital media and the Internet of Things.<br/><br/> 
+                    Present in more than 2.700 cities in the United States and abroad, the company provides internet, cable TV, landline and mobile telephony services. In addition, Verizon has been investing its efforts in interactive entertainment, digital media and the Internet of Things.<br/><br/> 
 
-            Verizon employs more than 153,000 people and serves 110.8 million mobile subscribers, 5.8 million cable TV customers and 6.9 million Internet consumers. 
-        </Text>
+                    Verizon employs more than 153,000 people and serves 110.8 million mobile subscribers, 5.8 million cable TV customers and 6.9 million Internet consumers. 
+                </Text>
             </Box>
 
     </Flex>
