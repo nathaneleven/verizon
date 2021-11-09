bf11c18b0397c0d65384264fe22669e8eebe3d1a
art-santos <arthursantosdeveloper@gmail.com>
2021-11-09 12:20:22 -0300

    reafactor: Hero && Plans changed

--- src/components/atoms/CallNowPhoneBt.jsx
+++ src/components/atoms/CallNowPhoneBt.jsx
@@ -5,7 +5,7 @@ import { Link } from '@chakra-ui/layout'
 const CallNowPhoneBt = () => {
   return (
     <>
-<Link href="tel:+18339803434"><Button colorScheme="white" size="lg" variant="outline" borderRadius="full" py="30px">Call Now<br/>(833) 980-3434</Button></Link>
+<Link href="tel:+18339803434"><Button colorScheme="white" size="lg" variant="outline" borderRadius="full" py="30px">1 (833) 980-3434</Button></Link>
     </>
   )}
 
